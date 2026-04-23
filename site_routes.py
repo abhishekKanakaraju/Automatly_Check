83601b4666461ccd788a47453b53908ceeb60468
Sai Ganesh Vankadara <sai.ganesh@spire.ai>
2026-04-23 12:26:11 +0530

    update 23-04-2026 12:26:11.35

--- site_routes.py
+++ site_routes.py
@@ -5,3 +5,11 @@ site_bp = Blueprint("site", __name__)
 @site_bp.route("/")
 def index():
     return render_template("index.html")
+
+@site_bp.route("/for-job-seekers")
+def job_seekers():
+    return render_template("iexchange-jobseekers.html")
+
+@site_bp.route("/for-employers")
+def employers():
+    return render_template("iexchange-employers.html")
